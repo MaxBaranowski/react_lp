bc8e038f2ebeff9b70b263c9d8d69eb026c991b1
Max Baranowski <max.baranowski@mail.ru>
2019-08-04 18:58:00 +0200

    build 04.08

---
+++ precache-manifest.08c2a1729f994965ae698e5584c66bf9.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f33c8d53288cfc01004aa91cd987d203",
+    "url": "/index.html"
+  },
+  {
+    "revision": "71c03bc19bc46897d683",
+    "url": "/static/css/main.70cd7f77.chunk.css"
+  },
+  {
+    "revision": "b03d560d9a34269239fb",
+    "url": "/static/js/2.f5564c02.chunk.js"
+  },
+  {
+    "revision": "71c03bc19bc46897d683",
+    "url": "/static/js/main.c06e56d0.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ede0bad8f57bb3c9f1e2c6c563625f31",
+    "url": "/static/media/circle_1.ede0bad8.png"
+  },
+  {
+    "revision": "8764d2344f324021486d98d343b66f1e",
+    "url": "/static/media/circle_2.8764d234.png"
+  },
+  {
+    "revision": "ac6f0422da6f5403baa30551664979a8",
+    "url": "/static/media/kpf.ac6f0422.png"
+  },
+  {
+    "revision": "26779049861e99a40ac05d1b8e8564d9",
+    "url": "/static/media/logo.26779049.png"
+  }
+]);
