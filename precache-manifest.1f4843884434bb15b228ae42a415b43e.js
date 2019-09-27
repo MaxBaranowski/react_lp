fd6d76a5bd8303b3c58ef95bda85ad1c601a0799
Max Baranowski <max.baranowski@mail.ru>
2019-09-27 19:37:53 +0300

    Updates

---
+++ precache-manifest.1f4843884434bb15b228ae42a415b43e.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "efcd660dbd228e452a29b6edabf0f691",
+    "url": "/react_lp/index.html"
+  },
+  {
+    "revision": "62029bb0c61190155b76",
+    "url": "/react_lp/static/css/2.58af8fe3.chunk.css"
+  },
+  {
+    "revision": "5e0ac723e82b750f9084",
+    "url": "/react_lp/static/css/main.3b1c4595.chunk.css"
+  },
+  {
+    "revision": "62029bb0c61190155b76",
+    "url": "/react_lp/static/js/2.b6191442.chunk.js"
+  },
+  {
+    "revision": "5e0ac723e82b750f9084",
+    "url": "/react_lp/static/js/main.e0c44c3c.chunk.js"
+  },
+  {
+    "revision": "ca6541e77b7241327d7c",
+    "url": "/react_lp/static/js/runtime~main.a8f04872.js"
+  },
+  {
+    "revision": "ede0bad8f57bb3c9f1e2c6c563625f31",
+    "url": "/react_lp/static/media/circle_1.ede0bad8.png"
+  },
+  {
+    "revision": "8764d2344f324021486d98d343b66f1e",
+    "url": "/react_lp/static/media/circle_2.8764d234.png"
+  },
+  {
+    "revision": "ac6f0422da6f5403baa30551664979a8",
+    "url": "/react_lp/static/media/kpf.ac6f0422.png"
+  },
+  {
+    "revision": "26779049861e99a40ac05d1b8e8564d9",
+    "url": "/react_lp/static/media/logo.26779049.png"
+  }
+]);
